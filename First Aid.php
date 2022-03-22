c5ac2cf69b74748f0a218f070a964e159889cdf7
lamim <slamim192079@bscse.uiu.ac.bd>
2022-03-22 17:38:28 +0600

    Create First Aid.php

---
+++ First Aid.php
@@ -0,0 +1,19 @@
+<html>
+        <head><title>First Aid</title></head>
+<body>
+<p><h1><p style="color:green">Emergency <i>First Aid  </i> Guide <h2><i>You can save someone's life....</i></h2></p></h1>
+
+
+<p><h3>We hope this guide will be helpful for you. 
+    Purpose of this guide is to prevent situations from worsening, 
+    by treating injuries and illnesses, reliving from pain.Also to provide 
+    physical and emotional support and to prevent the casualty from dying. 
+    Search above to see the specific chapter of our emergrncy FIRST AID GUIDE.If you still feel distressed or confused 
+    please call 999 for 24/7 available national online medical service services.
+    <h1><p style="color:red">HELP LINE: 999</p></h1>
+
+
+
+   
+</body>
+</html> 
